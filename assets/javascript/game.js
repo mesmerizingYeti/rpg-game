b684719879430dcfbbb0196035ea2257fbbf07ae
Nathanael Hauser <nathanaelhauser@gmail.com>
2019-09-02 15:23:46 -0700

    Added basic files & bootstrap elems to index.html

---
+++ assets/javascript/game.js
@@ -0,0 +1,39 @@
+let luke = { 
+    name: 'Luke Skywalker',
+    hp: 1,
+    attack: 1,
+    counter: 1,
+    img: './assets/images/luke-skywalker.png'
+}
+
+let obi = {
+    name: 'Obi-Wan Kenobi',
+    hp: 1,
+    attack: 1,
+    counter: 1,
+    img: './assets/images/obi-wan-kenobi.png'
+}
+
+let sidious = {
+    name: 'Darth Sidious',
+    hp: 1,
+    attack: 1,
+    counter: 1,
+    img: './assets/images/darth-sidious.png'
+}
+
+let maul = {
+    name: 'Darth Maul',
+    hp: 1,
+    attack: 1,
+    counter: 1,
+    img: './assets/images/darth-maul.png'
+}
+
+let characters = [luke, obi, sidious, maul]
+let chosenCharacter = -1
+let enemies = []
+
+const setupGame = _ => {
+    
+}
