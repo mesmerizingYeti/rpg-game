fcbfa4be42b6443d3e0b508eba54a2fade4f18d0
Nathanael Hauser <nathanaelhauser@gmail.com>
2019-09-03 15:08:19 -0700

    Added functionality to attack-btn

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,8 @@
 let obi = {
     name: 'Obi-Wan Kenobi',
     hp: 120,
-    attack: 1,
-    counter: 1,
+    attack: 8,
+    counter: 10,
     img: './assets/images/obi-wan-kenobi.png',
     cardID: 'obi-card'
 }
@@ -10,8 +10,8 @@ let obi = {
 let luke = {
     name: 'Luke Skywalker',
     hp: 100,
-    attack: 1,
-    counter: 1,
+    attack: 8,
+    counter: 5,
     img: './assets/images/luke-skywalker.png',
     cardID: 'luke-card'
 }
@@ -19,8 +19,8 @@ let luke = {
 let sidious = {
     name: 'Darth Sidious',
     hp: 150,
-    attack: 1,
-    counter: 1,
+    attack: 12,
+    counter: 20,
     img: './assets/images/darth-sidious.png',
     cardID: 'sidious-card'
 }
@@ -28,8 +28,8 @@ let sidious = {
 let maul = {
     name: 'Darth Maul',
     hp: 180,
-    attack: 1,
-    counter: 1,
+    attack: 15,
+    counter: 25,
     img: './assets/images/darth-maul.png',
     cardID: 'maul-card'
 }
@@ -38,23 +38,40 @@ const characters = [obi, luke, sidious, maul]
 let chosenCharacter = ''
 let defender = ''
 let enemies = []
+let attackMultiplier = 1
+let characterHP = 0
+let defenderHP = 0
+
+const restartGame = _ => {
+
+}
+
+const updateDocument = _ => {
+    if (chosenCharacter !== '') {
+        $(`#${chosenCharacter.cardID}-health`).text(characterHP)
+    }
+    if (defender !== '') {
+        $(`#${defender.cardID}-health`).text(defenderHP)
+    }
+}
+
+const addToLog = x => $('#fight-log').append(`<br>${x}`)
+const setLog = x => $('#fight-log').text(x)
 
 const chooseDefender = i => {
     defender = enemies[i]
+    defenderHP = enemies[i].hp
     enemies.splice(i, 1)
     $('#sixth-row').append($(`#${defender.cardID}`).remove())
     $(`#${defender.cardID}`).removeClass('bg-danger')
     $(`#${defender.cardID}`).addClass('bg-dark')
 
     updateDocument()
-
-    $('#attack-btn').click( function(event) {
-
-    })
 }
 
 const chooseCharacter = i => {
     chosenCharacter = characters[i]
+    characterHP = characters[i].hp
     enemies = characters
     enemies.splice(i, 1)
     $('#third-row').append($(`#${chosenCharacter.cardID}`).remove())
@@ -73,18 +90,42 @@ const chooseCharacter = i => {
     })
 }
 
-const updateDocument = _ => {
-    $(`#${chooseCharacter.cardID}-health`).text(chooseCharacter.hp)
-    if (defender !== '') {
-        $(`#${defender.cardID}-health`).text(defender.hp)
-    }
-    enemies.forEach( x => {
-        $(`#${x.cardID}-health`).text(x.hp)
-    })
-}
-
 $('#second-row .card').click( function(event) {
     characters.forEach( (x, i) => ($(this).attr('id') === x.cardID)?chooseCharacter(i):'')
+})
+
+$('#attack-btn').click( function(event) {
+    if (chosenCharacter !== '') {
+        if (defender !== '') {
+            
+            defenderHP -= chosenCharacter.attack * attackMultiplier
+            setLog(`You attacked ${defender.name} for ${chosenCharacter.attack * attackMultiplier}.`)
+            if (defenderHP <= 0) {
+                 alert('You won!')
+                if (enemies.length > 0) {
+                    setLog(`You have defeated ${defender.name}, choose another enemy to fight.`)
+                } else {
+                    setLog('You won!! Click restart to play again.')
+                    addToLog(`<button id="restart-btn" class="btn btn-primary" onclick="restartGame">Restart</button>`)
+                }
+                $(`#${defender.cardID}`).remove()
+                defender = ''
+            } else {
+                characterHP -= defender.counter
+                addToLog(`${defender.name} attacked you back for ${defender.counter}.`)
+                if (characterHP <= 0) {
+                    alert('You lose!')
+                }
+            }
+            attackMultiplier++
+            updateDocument()
+
+        } else {
+            alert('Choose a defender first!')
+        }
+    } else {
+        alert('Choose your character first!')
+    }
 })
 
 // $('#fourth-row .card').click( function(event) {
