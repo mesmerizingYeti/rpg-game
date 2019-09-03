7c151868273402b3169a7450925a3cab21b684e6
Nathanael Hauser <nathanaelhauser@gmail.com>
2019-09-03 04:54:30 -0700

    Added chooseDefender function in game.js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -39,6 +39,18 @@ let chosenCharacter = ''
 let defender = ''
 let enemies = []
 
+const chooseDefender = i => {
+    defender = enemies[i]
+    enemies.splice(i, 1)
+    $('#sixth-row').append($(`#${defender.cardID}`).remove())
+    $(`#${defender.cardID}`).removeClass('bg-danger')
+    $(`#${defender.cardID}`).addClass('bg-dark')
+
+    $('#attack-btn').click( function(event) {
+
+    })
+}
+
 const chooseCharacter = i => {
     chosenCharacter = characters[i]
     enemies = characters
@@ -47,20 +59,28 @@ const chooseCharacter = i => {
     enemies.forEach( x => $('#fourth-row').append($(`#${x.cardID}`).remove()) )
     $('#fourth-row .card').removeClass('bg-success')
     $('#fourth-row .card').addClass('bg-danger')
+
+    $('#fourth-row .card').click( function(event) {
+        console.log(event)
+        if (defender === '') {
+            console.log(event.currentTarget)
+            characters.forEach( (x, i) => (event.currentTarget.id === x.cardID)?chooseDefender(i):'')
+        }
+    })
 }
 
-const chooseDefender = i => {
-    defender = enemies[i]
-    enemies.splice(i, 1)
-    
+const updateDocument = _ => {
+    $(`#${chooseCharacter.cardID}`).
 }
 
 $('#second-row .card').click( function(event) {
     characters.forEach( (x, i) => ($(this).attr('id') === x.cardID)?chooseCharacter(i):'')
 })
 
-$('#fourth-row .card').click( function(event) {
-    if (defender !== '') {
-
-    }
-})
+// $('#fourth-row .card').click( function(event) {
+//     console.log(event)
+//     if (defender === '') {
+//         console.log('passed')
+//         characters.forEach( (x, i) => ($(this).attr('id' === x.cardID)?chooseDefender(i):''))
+//     }
+// })
