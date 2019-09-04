6948929ce9b824c1d654503bfab233fb6676ff51
Nathanael Hauser <nathanaelhauser@gmail.com>
2019-09-03 20:09:10 -0700

    Added comments and constants.js

---
+++ assets/javascript/constants.js
@@ -0,0 +1,31 @@
+const characters = [
+    {
+        name: 'Obi-Wan Kenobi',
+        hp: 120,
+        attack: 20,
+        counter: 10,
+        img: './assets/images/obi-wan-kenobi.png',
+        cardID: 'obi-card'
+    }, {
+        name: 'Luke Skywalker',
+        hp: 100,
+        attack: 8,
+        counter: 5,
+        img: './assets/images/luke-skywalker.png',
+        cardID: 'luke-card'
+    }, {
+        name: 'Darth Sidious',
+        hp: 150,
+        attack: 12,
+        counter: 20,
+        img: './assets/images/darth-sidious.png',
+        cardID: 'sidious-card'
+    }, {
+        name: 'Darth Maul',
+        hp: 180,
+        attack: 15,
+        counter: 25,
+        img: './assets/images/darth-maul.png',
+        cardID: 'maul-card'
+    }
+]
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,121 +1,83 @@
-let obi = {
-    name: 'Obi-Wan Kenobi',
-    hp: 120,
-    attack: 120,
-    counter: 10,
-    img: './assets/images/obi-wan-kenobi.png',
-    cardID: 'obi-card'
-}
-
-let luke = {
-    name: 'Luke Skywalker',
-    hp: 100,
-    attack: 8,
-    counter: 5,
-    img: './assets/images/luke-skywalker.png',
-    cardID: 'luke-card'
-}
-
-let sidious = {
-    name: 'Darth Sidious',
-    hp: 150,
-    attack: 12,
-    counter: 20,
-    img: './assets/images/darth-sidious.png',
-    cardID: 'sidious-card'
-}
-
-let maul = {
-    name: 'Darth Maul',
-    hp: 180,
-    attack: 15,
-    counter: 25,
-    img: './assets/images/darth-maul.png',
-    cardID: 'maul-card'
-}
-
-const characters = [obi, luke, sidious, maul]
 let chosen = -1
 let defender = -1
-let unselected = [0, 1, 2, 3]
+let unselected = []
 let attackMultiplier = 1
 let characterHP = 0
 let defenderHP = 0
 
-
-
-
 const addToLog = x => $('#fight-log').append(`<br>${x}`)
 const setLog = x => $('#fight-log').text(x)
 const updateDocument = _ => {
-    console.log(`Chosen: ${chosen}, Defender: ${defender}`)
-    if (chosen === -1) {
-        $(`#${characters[chosen].cardID}-health`).text(characterHP)
-    }
-    (chosen !== -1) ? $(`#${characters[chosen].cardID}-health`).text(characterHP) : ''
-    (defender !== -1) ? $(`#${characters[defender].cardID}-health`).text(defenderHP) : ''
+    chosen !== -1 ? $(`#${characters[chosen].cardID}-health`).text(characterHP) : ''
+    defender !== -1 ? $(`#${characters[defender].cardID}-health`).text(defenderHP) : ''
 }
 
 const chooseDefender = event => {
+    // Iterate thru characters and find the defender using event
     characters.forEach((x, i) => {
         if (event.currentTarget.id === x.cardID) {
             defender = i
             defenderHP = characters[i].hp
+            // Remove defender from unselected
             unselected.splice(unselected.indexOf(defender), 1)
-            console.log(unselected)
+            // Move defender's card to sixth-row
             $('#sixth-row').append($(`#${characters[defender].cardID}`).remove())
             $(`#${characters[defender].cardID}`).removeClass('bg-danger')
             $(`#${characters[defender].cardID}`).addClass('bg-dark')
-
-            updateDocument()
         }
     })
+    updateDocument()
 }
 
 const chooseCharacter = event => {
+    // Iterate thru characters and find the chosen character using event
     characters.forEach((x, i) => {
         if (event.currentTarget.id === x.cardID) {
             chosen = i
             characterHP = characters[i].hp
+            // Remove chosen charcter from unselected
             unselected.splice(unselected.indexOf(chosen), 1)
-            console.log(unselected)
+            // Move chosen's card to third-row
             $('#third-row').append($(`#${characters[chosen].cardID}`).remove())
+            // Move all other cards to fourth-row
             unselected.forEach(y => $('#fourth-row').append($(`#${characters[y].cardID}`).remove()))
             $('#fourth-row .card').removeClass('bg-success')
             $('#fourth-row .card').addClass('bg-danger')
-
-            updateDocument()
-
+            // Add click function to card's in fourth-row
             $('#fourth-row .card').click(function (event) {
                 (defender === -1) ? chooseDefender(event) : ''
             })
         }
     })
+    updateDocument()
 }
 
 $('#attack-btn').click(function (event) {
-    console.log(unselected)
     if (chosen !== -1) {
         if (defender !== -1) {
-
+            // Chosen attacks defender
             defenderHP -= characters[chosen].attack * attackMultiplier
             setLog(`You attacked ${characters[defender].name} for ${characters[chosen].attack * attackMultiplier}.`)
+            // Defender is defeated
             if (defenderHP <= 0) {
                 alert('You won!')
                 if (unselected.length > 0) {
                     setLog(`You have defeated ${characters[defender].name}, choose another enemy to fight.`)
                 } else {
+                    // Win condition - all enemies defeated
                     setLog('You won!! Click restart to play again.')
+                    // Add restart game button
                     addToLog(`<button id="restart-btn" class="btn btn-primary">Restart</button>`)
-
                     $('#restart-btn').click(_ => restartGame())
                 }
                 $(`#${characters[defender].cardID}`).remove()
                 defender = -1
             } else {
+                // Defender counter attacks
                 characterHP -= characters[defender].counter
                 addToLog(`${characters[defender].name} attacked you back for ${characters[defender].counter}.`)
                 if (characterHP <= 0) {
+                    // Lose condition
                     alert('You lose!')
                 }
             }
@@ -131,14 +93,18 @@ $('#attack-btn').click(function (event) {
 })
 
 const restartGame = _ => {
-    console.log('restartGame')
     chosen = -1
     defender = -1
-    unselected = [0, 1, 2, 3]
+    // Initialize array for indices of all characters
+    unselected = []
+    for (let i = 0; i < characters.length; i++) {
+        unselected.push(i)
+    }
     attackMultiplier = 1
     $('#restart-btn').remove()
     $('.card').remove()
     setLog('')
+    // Add card for each character to second-row
     characters.forEach(x => {
         $('#second-row').append(`
             <div class="card shadow text-center bg-success text-white" id="${x.cardID}">
@@ -151,6 +117,7 @@ const restartGame = _ => {
         `)
 
     })
+    // Add click function to every card in second-row
     $(`#second-row .card`).click(function (event) {
         chooseCharacter(event)
     })
