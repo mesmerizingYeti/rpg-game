528d1d7a6fbc458f7c4fa4796d558a2272d23261
Nathanael Hauser <nathanaelhauser@gmail.com>
2019-09-10 14:27:33 -0700

    Finished choosePokemon for chosen & defender

--- assets/javascript/constants.js
+++ assets/javascript/constants.js
@@ -5,27 +5,31 @@ const characters = [
         attack: 20,
         counter: 10,
         img: './assets/images/obi-wan-kenobi.png',
-        cardID: 'obi-card'
+        cardID: 'obi-card',
+        id: 'obi-wan-kenobi'
     }, {
         name: 'Luke Skywalker',
         hp: 100,
         attack: 8,
         counter: 5,
         img: './assets/images/luke-skywalker.png',
-        cardID: 'luke-card'
+        cardID: 'luke-card',
+        id: 'luke-skywalker'
     }, {
         name: 'Darth Sidious',
         hp: 150,
         attack: 12,
         counter: 20,
         img: './assets/images/darth-sidious.png',
-        cardID: 'sidious-card'
+        cardID: 'sidious-card',
+        id: 'darth-sidious'
     }, {
         name: 'Darth Maul',
         hp: 180,
         attack: 15,
         counter: 25,
         img: './assets/images/darth-maul.png',
-        cardID: 'maul-card'
+        cardID: 'maul-card',
+        id: 'darth-maul'
     }
 ]
--- assets/javascript/pokemonVersion.js
+++ assets/javascript/pokemonVersion.js
@@ -1,7 +1,7 @@
 let attackMultiplier = 1
 let chosen = -1
 let defender = -1
-let unselected = []
+let unselected = [0, 1, 2, 3]
 
 const hide = x => {
     x.css('height', '0')
@@ -14,32 +14,113 @@ const show = x => {
 
 // Choose pokemon
 const choosePokemon = _ => {
+    console.log('running choosePokemon')
     // Show correct div
-    hide($('div').not('.container'))
-    show($('#choose-screen'))
-    // Populate div
+    hide($('.row').not('.container'))
+    show($('#choose-row'))
+    // Populate div with unselected characters
     unselected.forEach(x => {
+        let char = characters[x]
+        let pokeballID = `${char.id}-pokeball`
+        // Add pokeball for character
         $('#choose-screen').append(`
-            
+            <div id="${char.id}-container">
+                <img src="./assets/images/pokeball-still.png" alt="Pokeball" class="px-4 no-outline" id="${pokeballID}" data-index="${x}">
+            </div>
         `)
+        // Add hover animation for pokeball
+        $(`#${pokeballID}`).hover(
+            function() {
+                $(`#${pokeballID}`).attr('src', './assets/images/pokeball-moving.gif')
+            }, function() {
+                $(`#${pokeballID}`).attr('src', './assets/images/pokeball-still.png')
+            }
+        )
+        // Add tooltip for pokeball
+        tippy(`#${pokeballID}`, {
+            arrow: true,
+            animation: 'fade',
+            allowHTML: true,
+            content(reference) {
+                console.log(char)
+                let popover = document.createElement('div')
+                popover.innerHTML = `
+                    <img src=${char.img} alt="${char.name}" class="popover-img">
+                    <br>
+                    <strong>${char.name}</strong>
+                `
+                return popover
+            }
+        })
+        console.log(`Adding ${char.name}`)
     })
+    // Clicking will assign character to chosen or defender
+    $(`[id$=-pokeball]`).click(function(event) {
+        let index = this.dataset.index
+        // If chosen dosen't exist, else
+        if (chosen === -1) {
+            chosen = index
+            $('#choose-screen-title').html(`<h2>Choose your Enemy!</h2>`)
+        } else {
+            defender = index
+        }
+        unselected.splice(unselected.indexOf(index), 1)
+        $(this.parentNode).remove()
+        console.log(`Chosen: ${chosen}, Defender: ${defender}`)
+    })
+}
 
+// ***** Battle *****
 
-    // Add click functionality
+// hide choose-screen
+// show battle-screen
+// setup battle-screen
+// animations for both attack and counter-attack
+// 4 buttons with text log
+// 1. Attack:  attack opponent, receive counter attack
+// 2. Item:    nothing, some snarky reply
+// 3. Pokemon: nothing (unless have time)
+// 4. Run:     can not escape
 
+// If win battle, then go back to choosePokemon()
+// If lose battle, then lose
+
+// ***** Win *****
+
+// All opponents lost, celebrate then reset game
+const win = _ => {
+    swal({
+        Title: 'You Won!',
+        text: 'You defeated all your opponents! Good job!',
+        icon: 'success'
+    })
+    resetGame()
+}
+
+// ***** Lose *****
+
+// Lost a battle, mock player then reset game
+const lose = _ => {
+    swal({
+        title: 'You Lost!',
+        text: 'You should just give up!',
+        icon: 'error'
+    })
+    resetGame()
+}
+
+// ***** Reset *****
+
+// Reset variables, then run choosePokemon()
+const resetGame = _ => {
+    attackMultiplier = 1
+    chosen = -1
+    defender = -1
+    unselected = [0, 1, 2, 3]
+    $('#choose-screen-title').html(`<h2>Choose your Character!</h2>`)
+    choosePokemon()
 }
 
 
-// Choose opponent
-
-// Battle
-
-// Win
-
-// Lose
-
-// Reset
-
-
-show($('#choose-screen'))
-hide($('#battle-screen'))
+// Start game
+resetGame()
