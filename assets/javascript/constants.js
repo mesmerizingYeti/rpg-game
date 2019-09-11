556cd8dd176d6e7b7f8bcfbc02f374afca2adb35
Nathanael Hauser <nathanaelhauser@gmail.com>
2019-09-11 14:00:32 -0700

    Finished choosePokemon logic, added battle logic

--- assets/javascript/constants.js
+++ assets/javascript/constants.js
@@ -3,14 +3,14 @@ const characters = [
         name: 'Obi-Wan Kenobi',
         hp: 120,
         attack: 20,
-        counter: 10,
+        counter: 5,
         img: './assets/images/obi-wan-kenobi.png',
         cardID: 'obi-card',
         id: 'obi-wan-kenobi'
     }, {
         name: 'Luke Skywalker',
         hp: 100,
-        attack: 8,
+        attack: 20,
         counter: 5,
         img: './assets/images/luke-skywalker.png',
         cardID: 'luke-card',
@@ -18,16 +18,16 @@ const characters = [
     }, {
         name: 'Darth Sidious',
         hp: 150,
-        attack: 12,
-        counter: 20,
+        attack: 20,
+        counter: 5,
         img: './assets/images/darth-sidious.png',
         cardID: 'sidious-card',
         id: 'darth-sidious'
     }, {
         name: 'Darth Maul',
         hp: 180,
-        attack: 15,
-        counter: 25,
+        attack: 20,
+        counter: 5,
         img: './assets/images/darth-maul.png',
         cardID: 'maul-card',
         id: 'darth-maul'
--- assets/javascript/pokemonVersion.js
+++ assets/javascript/pokemonVersion.js
@@ -1,6 +1,8 @@
 let attackMultiplier = 1
 let chosen = -1
 let defender = -1
+let chosenHP = 0
+let defenderHP = 0
 let unselected = [0, 1, 2, 3]
 
 const hide = x => {
@@ -12,12 +14,45 @@ const show = x => {
     x.css('visibility', 'visible')
 }
 
+const startBattle = event => {
+    console.log('starting battle')
+    show($(`.battle-options-row`))
+    $('#battle-log').text(`What will ${characters[chosen].name.toUpperCase()} do?`)
+    document.getElementsByTagName('body')[0].removeEventListener('click', startBattle)
+
+    $('.battle-option').click(function (event) {
+        console.log(this)
+        switch ($(this).text()) {
+            case 'FIGHT':
+                $('#battle-log').text(`${characters[chosen].name} used punch!`)
+                defenderHP -= attackMultiplier*characters[chosen].attack
+                defenderHP < 0 ? '' : ''
+                setTimeout(_ => {
+                    $('#battle-log').text(`${characters[defender].name} countered with bitch slap!`)
+                }, 1000)
+                break
+            case 'BAG':
+                $('#battle-log').text(`You're too poor to buy items!`)
+                break
+            case 'POKéMON':
+                $('#battle-log').text(`You're too poor to feed more pokémon!`)
+                break
+            case 'RUN':
+                $('#battle-log').text(`Can't escape!`)
+                break
+            default:
+                break
+        }
+    })
+}
+
 // Choose pokemon
 const choosePokemon = _ => {
     console.log('running choosePokemon')
     // Show correct div
     hide($('.row').not('.container'))
     show($('#choose-row'))
+    hide($('.battle-options-row'))
     // Populate div with unselected characters
     unselected.forEach(x => {
         let char = characters[x]
@@ -30,9 +65,9 @@ const choosePokemon = _ => {
         `)
         // Add hover animation for pokeball
         $(`#${pokeballID}`).hover(
-            function() {
+            function () {
                 $(`#${pokeballID}`).attr('src', './assets/images/pokeball-moving.gif')
-            }, function() {
+            }, function () {
                 $(`#${pokeballID}`).attr('src', './assets/images/pokeball-still.png')
             }
         )
@@ -55,35 +90,77 @@ const choosePokemon = _ => {
         console.log(`Adding ${char.name}`)
     })
     // Clicking will assign character to chosen or defender
-    $(`[id$=-pokeball]`).click(function(event) {
+    $(`[id$=-pokeball]`).click(function (event) {
         let index = this.dataset.index
-        // If chosen dosen't exist, else
-        if (chosen === -1) {
-            chosen = index
-            $('#choose-screen-title').html(`<h2>Choose your Enemy!</h2>`)
-        } else {
-            defender = index
-        }
         unselected.splice(unselected.indexOf(index), 1)
         $(this.parentNode).remove()
         console.log(`Chosen: ${chosen}, Defender: ${defender}`)
+        // If chosen dosen't exist, else
+        if (chosen === -1) {
+            chosen = index
+            chosenHP = characters[chosen].hp
+            $('#choose-screen-title').text(`<h2>Choose your Enemy!</h2>`)
+        } else {
+            defender = index
+            defenderHP = characters[defender].hp
+            battle()
+        }
     })
 }
 
 // ***** Battle *****
 
+const battle = _ => {
+    console.log('running battle')
+    // Show correct divs
+    hide($('.row').not('.container'))
+    show($('#battle-row'))
+    console.log($('#battle-log-container'))
+    show($('#battle-log-container'))
+    // Create characters
+    let chosenChar = `
+        <div id="chosen-container">
+            <img src="./assets/images/${characters[chosen].id}-cartoon.png" alt="chosen" id="chosen-img" class="character-img">
+        </div>
+    `
+    let defenderChar = `
+        <div id="defender-container">
+            <img src="./assets/images/${characters[defender].id}-cartoon.png" alt="defender" id="defender-img" class="character-img">
+        </div>
+    `
+    $(`#battle-row`).append(chosenChar, defenderChar)
+    // Setup battle text-box
+    $('#battle-log').text(`Wild ${characters[defender].name.toUpperCase()} appeared!`)
+    setTimeout(_ => document.getElementsByTagName('body')[0].addEventListener('click', startBattle), 300)
+
+}
+
 // hide choose-screen
 // show battle-screen
 // setup battle-screen
-// animations for both attack and counter-attack
-// 4 buttons with text log
-// 1. Attack:  attack opponent, receive counter attack
-// 2. Item:    nothing, some snarky reply
-// 3. Pokemon: nothing (unless have time)
-// 4. Run:     can not escape
+
+// ---1---
+// Defender moves in from left
+// Player moves in from right
+// Defender health bar moves in from left faster
+// Defender animation/makes noise
+// "Wild DEFENDER-NAME appeared!(Bouncing arrow)"
+
+// ---2---
+// Go! CHOSEN-NAME!
+// Throw pokeball
+// Chosen health bar moves in from right faster & chosen animation/makes noise
+// `What will               FIGHT       BAG
+//  CHOSEN-NAME do?`        POKeMON     RUN
+
+
 
 // If win battle, then go back to choosePokemon()
 // If lose battle, then lose
+const winBattle = _ => {
+        unselected.length < 1 ? win() : choosePokemon()
+}
+
 
 // ***** Win *****
 
@@ -116,6 +193,8 @@ const resetGame = _ => {
     attackMultiplier = 1
     chosen = -1
     defender = -1
+    chosenHP = 0
+    defenderHP = 0
     unselected = [0, 1, 2, 3]
     $('#choose-screen-title').html(`<h2>Choose your Character!</h2>`)
     choosePokemon()
